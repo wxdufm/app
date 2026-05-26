4aada356b8afe1a56290611d6ddfabdc9f9e6e89
hannah.shin@duke.edu <hs451@duke.edu>
2026-05-26 17:16:40 -0400

    attempting again to add stream live button to homepage

--- pages/index.js
+++ pages/index.js
@@ -37,12 +37,9 @@ export default function Home(props) {
 								</div>
 								{/* Stream button */}
 								<div className="mt-4 flex justify-center">
-									
-										href="https://stream.wxdu.art/wxdu192.mp3"
-										target="_blank"
-										className="kallistobold bg-red-600 px-6 py-2 text-white hover:bg-red-400"
-									>
+									<a href="https://stream.wxdu.art/wxdu192.mp3" target="_blank" className="kallistobold bg-red-600 px-6 py-2 text-white hover:bg-red-400">
 										▶ Stream Live
+									</a>
 								</div>
 							</div>
 						</div>
