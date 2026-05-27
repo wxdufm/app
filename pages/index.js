5dfade1ea40d82c81be2d2461e950f68a00d600f
joseph.yan@duke.edu <jy414@duke.edu>
2026-05-27 15:57:27 -0400

    fixed recent sorting of blog preview, created nonfound.png fallback for blog previews missing images

--- pages/index.js
+++ pages/index.js
@@ -96,7 +96,7 @@ export const getStaticProps = async () => {
 		query: `
     query getContent($startOfWeek: String, $endOfWeek: String)
     {    
-        blogConnection(sort: "published", last:6, before: "cG9zdCNkYXRlIzE2NTc4Njg0MDAwMDAjY29udGVudC9wb3N0cy9hbm90aGVyUG9zdC5qc29u"){
+        blogConnection(sort: "published", last:6){
           edges {
             node {
               id
