6b3733c13a76eeff95d9b86fe6a55f06d4dfd93a
jojojojo <jy414@duke.edu>
2026-06-02 14:20:05 -0400

    changed slug to enable static loading of weekly schedule table

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,6 +1,7 @@
 import {TinaMarkdown} from 'tinacms/dist/rich-text'
 import {tinaField, useTina} from 'tinacms/dist/react'
 import {client} from '../tina/__generated__/client'
+import WeeklySchedule from '../components/WeeklySchedule'
 
 //editable static pages (programming, contact, etc.)
 export default function Home(props) {
@@ -13,13 +14,23 @@ export default function Home(props) {
 
 	// store whatever is in rich text editor for that page in variable content
 	const content = data.page.body
+	const isProgrammingPage = props.slug === 'programming'
+
+	// adds a Tina component for weekly schedule
+	const components = {
+		weeklySchedule: () => isProgrammingPage ? (
+			<div className="not-prose relative left-1/2 mt-8 w-[80vw] -translate-x-1/2">
+				<WeeklySchedule schedule={props.schedule} />
+			</div>
+		) : null
+	}
 
 	return (
 		<div>
 			<div data-tina-field={tinaField(data.page, 'body')}>
-				<div className="mx-auto flex w-5/6 flex-row justify-center pb-10">
+				<div className="mx-auto flex w-full flex-col items-center pb-10">
 					<article className="prose prose-lg text-white prose-h1:font-kallisto prose-h1:font-normal prose-h1:text-white prose-h3:text-gray-400 prose-a:text-blue-500 prose-strong:text-slate-700">
-						<TinaMarkdown content={content} />
+						<TinaMarkdown content={content} components={components} />
 					</article>
 				</div>
 			</div>
@@ -42,12 +53,26 @@ export const getStaticProps = async (ctx) => {
 	const {data, query, variables} = await client.queries.page({
 		relativePath: ctx.params.slug + '.mdx',
 	})
+	// loads schedule data from csv file (via lib/schedule.js) if on programming page
+	// otherwise schedule is null and WeeklySchedule component won't render!
+	let schedule = null
+	if (ctx.params.slug === 'programming') {
+		const scheduleModule = require('../lib/schedule')
+		const loadSchedule =
+			scheduleModule.loadSchedule ||
+			scheduleModule.default?.loadSchedule ||
+			scheduleModule.default
+
+		schedule = typeof loadSchedule === 'function' ? loadSchedule() : null
+	}
 
 	return {
 		props: {
 			data,
 			query,
 			variables,
+			slug: ctx.params.slug,
+			schedule,
 		},
 	}
 }
