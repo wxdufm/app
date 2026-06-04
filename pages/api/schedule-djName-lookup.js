ba25ae730516d11a7a6399872d8d41738ca6f2c0
jojojojo <jy414@duke.edu>
2026-06-04 19:13:20 -0400

    temp

--- pages/api/schedule-djName-lookup.js
+++ pages/api/schedule-djName-lookup.js
@@ -1,48 +1,14 @@
-// open database connection from lib/db/plmanager.js
+import { lookupDjNamesFromIDs } from 'lib/schedule/djName-lookup'
 
-// query users in the plmanager database, using ID, returning defdjname
-    // use fallback of "[NO DJ NAME FOUND]" if no match found in users
-
-import db from "../../lib/db/plmanager";
+// receives body-only array of IDs
 
 export default async function handler(req, res) {
-    if (req.method !== "POST") return res.status(405).end();
-
-    const fullIdArray = req.body;
-    const djNameGrid = [];
-
-    for (const row of fullIdArray) {
-        const djNameRow = [];
-
-        for (let cell of row) {
-
-            // defaults empty cells to Otto, whose ID is 346
-            if (!cell || !cell.trim()) {
-                djNameRow.push(346)
-                continue;
-            }
-
-            // skips over errors generated in fullName-lookup
-            if ("[" in cell) {
-                djNameRow.push(cell);
-                continue;
-            }
-
-            // query plmanager
-            const [rows] = await db.query(
-                "SELECT ID FROM users WHERE id = ? LIMIT 1",
-                [cell]
-            );
-
-            if (rows.length > 0) {
-                djNameRow.push(rows[0].defdjname);
-            } else {
-                djNameRow.push(`[NO DJ NAME FOUND] ${cell}`);
-            }
-        }
-
-        djNameGrid.push(djNameRow);
+    if (req.method !== "POST") return res.status(405).end()
+    try {
+        const idGrid = req.body;
+        const djNameGrid = await lookupDjNamesFromIDs(idGrid)
+        res.status(200).json(djNameGrid);
+    } catch (err) {
+        res.status(500).json({ error: "DJ Name lookup failed"})
     }
-
-    res.status(200).json(djNameGrid);
 }
