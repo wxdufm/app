bdb5dbd6af28d3e493faf4375f37bb295fb6783c
cdk49 <cdk49@duke.edu>
2026-06-09 15:07:06 -0400

    made the current playlist tab (last played)

--- components/listenpage/PlayTabs.js
+++ components/listenpage/PlayTabs.js
@@ -4,7 +4,7 @@ import {useState} from "react";
 import NowPlaying from "./NowPlaying";
 import LastPlayed from "./LastPlayed";
 
-export default function PlayTabs({ nowPlaying = {}}) {
+export default function PlayTabs({ nowPlaying = {}, currentPlaylist = {}}) {
     const [activeTab, setActiveTab] = useState("nowplaying");
 
     // To render the correct tab based on the activeTab state (i.e which tab the user has clicked on)
@@ -12,20 +12,24 @@ export default function PlayTabs({ nowPlaying = {}}) {
         if (activeTab === "nowplaying") {
             return <NowPlaying nowPlaying={nowPlaying} />
         } else if (activeTab === "lastplayed") {
-            return <LastPlayed />
+            return <LastPlayed currentPlaylist={currentPlaylist}/>
         }
     } 
 
     return(
-        <div role="tablist" className="text-center w-full max-w-[360px] mx-auto">
-            <button role="tab" onClick={() => setActiveTab("nowplaying")} aria-selected={activeTab === 'nowplaying'} className={activeTab === "nowplaying" ? "text-white border-b-2 border-blue-400 px-4 py-1 text-lg" : "text-gray-300 hover:text-white transition-colors duration-150 px-4 py-1 text-lg"}>
-                Now Playing
-            </button>
-            <button role="tab" onClick={() => setActiveTab("lastplayed")} aria-selected={activeTab === 'lastplayed'} className={activeTab === "lastplayed" ? "text-white border-b-2 border-blue-400 px-4 py-1 text-lg" : "text-gray-300 hover:text-white transition-colors duration-150 px-4 py-1 text-lg"}>
-                Last Played
-            </button>
-            {renderTab()}
+        <div className="w-full max-w-[360px] mx-auto">
+            <div role="tablist" className="flex justify-center">
+                <button role="tab" onClick={() => setActiveTab("nowplaying")} aria-selected={activeTab === 'nowplaying'} className={activeTab === "nowplaying" ? "text-white border-b-2 border-blue-400 px-4 py-2 text-lg" : "text-gray-300 hover:text-white transition-colors duration-150 px-4 py-2 text-lg"}>
+                    Now Playing
+                </button>
+                <button role="tab" onClick={() => setActiveTab("lastplayed")} aria-selected={activeTab === 'lastplayed'} className={activeTab === "lastplayed" ? "text-white border-b-2 border-blue-400 px-4 py-2 text-lg" : "text-gray-300 hover:text-white transition-colors duration-150 px-4 py-2 text-lg"}>
+                    Last Played
+                </button>
+            </div>
 
+            <div className="max-h-[60vh] overflow-y-auto">
+                {renderTab()}
+            </div>
         </div>
     )
 }
--- pages/listen/index.js
+++ pages/listen/index.js
@@ -16,6 +16,8 @@ export default function Listen() {
         dj: null
     })
 
+    const [currentPlaylist, setCurrentPlaylist] = useState({});
+
     // function to fetch now-playing data
     async function fetchNowPlaying() {
         try {
@@ -35,14 +37,32 @@ export default function Listen() {
         }
     }
 
+    // function to fetch current playlist
+    async function fetchCurrentPlaylist(){
+        try {
+            const response = await fetch('https://api.wxdu.art/api/playlists/current')
+
+            const data = await response.json()
+
+            setCurrentPlaylist(data)
+
+        } catch (error) {
+            console.error('Failed to fetch current-playlist data:', error)
+        }
+    }
+
     // calls API on component mount and every 30 seconds thereafter
     useEffect(() => {
 
         // initial fetch
-        fetchNowPlaying()
+        fetchNowPlaying();
+        fetchCurrentPlaylist();
 
         // poll API again every 30 secs
-        const interval = setInterval(fetchNowPlaying, 30000)
+        const interval = setInterval(() => {
+            fetchNowPlaying();
+            fetchCurrentPlaylist();
+        }, 3000)
 
         // clean-up on component unmount
         return () => clearInterval(interval)
@@ -56,7 +76,7 @@ export default function Listen() {
             <div className="grid grid-cols-1 md:grid-cols-[40%_60%] gap-8">
                 <div className="md:h-[calc(100vh-160px)] md:overflow-auto h-auto flex justify-center">
                     <div className="w-full max-w-[360px]">
-                        <PlayTabs nowPlaying={nowPlaying} />
+                        <PlayTabs nowPlaying={nowPlaying} currentPlaylist={currentPlaylist}/>
                     </div>
                 </div>
 
