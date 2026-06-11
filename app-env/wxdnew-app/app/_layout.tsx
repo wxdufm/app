4cbb7f7193d16b9e9a92ce56ab4854d1893073f0
Benjamin Ochoa <benjamin.ochoa@duke.edu>
2026-06-11 15:15:23 -0400

    added audioprovider to _layout

--- app-env/wxdnew-app/app/_layout.tsx
+++ app-env/wxdnew-app/app/_layout.tsx
@@ -4,15 +4,18 @@ This is the root of the entire app, equivalent to _app.js in Next.js
 
 import "../global.css";
 import { Tabs } from "expo-router";
+import { AudioProvider } from "../components/AudioContext";
 
 export default function RootLayout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: "Home" }} />
-            <Tabs.Screen name="listen/index" options={{ title: "Listen" }} />
-            <Tabs.Screen name="blog/index" options={{ title: "Blog" }} />
-            <Tabs.Screen name="charts/index" options={{ title: "Charts" }} />
-            <Tabs.Screen name="archive/index" options={{ title: "Archive" }} />
-        </Tabs>
+        <AudioProvider>
+            <Tabs>
+                <Tabs.Screen name="index" options={{ title: "Home" }} />
+                <Tabs.Screen name="listen/index" options={{ title: "Listen" }} />
+                <Tabs.Screen name="blog/index" options={{ title: "Blog" }} />
+                <Tabs.Screen name="charts/index" options={{ title: "Charts" }} />
+                <Tabs.Screen name="archive/index" options={{ title: "Archive" }} />
+            </Tabs>
+        </AudioProvider>
     );
 }
