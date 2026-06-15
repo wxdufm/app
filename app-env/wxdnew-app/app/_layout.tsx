5ace94ea419c8f33e3e0ffeaf1d9c8081601e3d3
Benjamin Ochoa <benjamin.ochoa@duke.edu>
2026-06-15 14:49:34 -0400

    added blog and archive / _layout.tsx. Also fixed app/_layout.tsx

--- app-env/wxdnew-app/app/_layout.tsx
+++ app-env/wxdnew-app/app/_layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout() {
             <Tabs>
                 <Tabs.Screen name="index" options={{ title: "Home" }} />
                 <Tabs.Screen name="listen/index" options={{ title: "Listen" }} />
-                <Tabs.Screen name="blog/index" options={{ title: "Blog" }} />
+                <Tabs.Screen name="blog" options={{ title: "Blog" }} />
                 <Tabs.Screen name="charts/index" options={{ title: "Charts" }} />
-                <Tabs.Screen name="archive/index" options={{ title: "Archive" }} />
+                <Tabs.Screen name="archive" options={{ title: "Archive" }} />
             </Tabs>
         </AudioProvider>
     );
